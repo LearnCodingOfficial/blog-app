c6cda28418f7c175a9a1c13b008a6e80deb737be
Learn Coding <90298313+LearnCodingOfficial@users.noreply.github.com>
2024-08-30 22:45:04 +0530

    AuthProvider.jsx

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [blogs, setBlogs] = useState();
@@ -9,16 +10,20 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const { data } = await axios.get(
-          "http://localhost:4001/api/users/my-profile",
-          {
-            withCredentials: true,
-            headers: { "Content-Type": "application/json" },
-          }
-        );
-        console.log(data);
-        setProfile(data);
-        setIsAuthenticated(true);
+        const token = Cookies.get("token");
+        const parsedToken = token ? JSON.parse(token) : undefined;
+        if (parsedToken) {
+          const { data } = await axios.get(
+            "http://localhost:4001/api/users/my-profile",
+            {
+              withCredentials: true,
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+          console.log(data);
+          setProfile(data);
+          setIsAuthenticated(true);
+        }
       } catch (error) {
         console.log(error);
       }
