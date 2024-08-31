36dad43e301acedf4eec75678dde6d3e821a5e3a
Learn Coding <90298313+LearnCodingOfficial@users.noreply.github.com>
2024-08-31 22:37:21 +0530

    AuthProvider.jsx

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
+
 export const AuthContext = createContext();
+
 export const AuthProvider = ({ children }) => {
   const [blogs, setBlogs] = useState();
   const [profile, setProfile] = useState();
@@ -10,14 +12,18 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const token = Cookies.get("jwt");
-        const parsedToken = token ? JSON.parse(token) : undefined;
-        if (parsedToken) {
+        const token = Cookies.get("jwt"); // Retrieve the token directly as a string
+       
+
+        if (token) {
           const { data } = await axios.get(
             "http://localhost:4001/api/users/my-profile",
             {
               withCredentials: true,
-              headers: { "Content-Type": "application/json" },
+              headers: {
+                Authorization: `Bearer ${token}`, // Passing the token in the Authorization header
+                "Content-Type": "application/json",
+              },
             }
           );
           console.log(data);
@@ -28,6 +34,7 @@ export const AuthProvider = ({ children }) => {
         console.log(error);
       }
     };
+
     const fetchBlogs = async () => {
       try {
         const { data } = await axios.get(
@@ -40,9 +47,11 @@ export const AuthProvider = ({ children }) => {
         console.log(error);
       }
     };
+
     fetchBlogs();
     fetchProfile();
   }, []);
+
   return (
     <AuthContext.Provider
       value={{
